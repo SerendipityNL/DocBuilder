6612817df20f9cbc5095b9dbcd01f713330eae8e
Douwe de Haan <douwedehaan@live.nl>
2013-03-18 12:18:08 +0100

    Changed some functions to the new conventions

--- test-users/app/models/user.js
+++ test-users/app/models/user.js
@@ -2,44 +2,44 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/blocknodes');
 
 var schema = mongoose.Schema
-  , object_id = schema.ObjectId;
+  , objectId = schema.ObjectId;
 
 
 var crypto = require('crypto');
 
 mongoose.model('User', {
 
-
- setters: {
-    password: function(password) {
-      this._password = password;
-      this.salt = this.makeSalt();
-      this.hashed_password = this.encryptPassword(password);
-    }
-  },
-
-  methods: {
-    authenticate: function(plainText) {
-      return this.encryptPassword(plainText) === this.hashed_password;
-    },
-
-    makeSalt: function() {
-      return Math.round((new Date().valueOf() * Math.random())) + '';
-    },
-
-    encryptPassword: function(password) {
-      return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
-    },
-
-    save: function(okFn, failedFn) {
-      if (this.isValid()) {
-        this.__super__(okFn);
-      } else {
-        failedFn();
-      }
-    }
-
-  }
+	setters: {
+		password: function(password) {
+			this._password = password;
+			this.salt = this.makeSalt();
+			this.hashed_password = this.encryptPassword(password);
+		}
+	},
+	
+	methods: {
+		authenticate: function(plainText) {
+			return this.encryptPassword(plainText) === this.hashed_password;
+		},
+	
+		makeSalt: function() {
+			return Math.round((new Date().valueOf() * Math.random())) + '';
+		},
+		
+		encryptPassword: function(password) {
+			return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+		},
+		
+		save: function(okFn, failedFn) {
+			if (this.isValid()) {
+				this.__super__(okFn);
+			}
+			else {
+				failedFn();
+			}
+		}
+	
+	}
 });
 
 
