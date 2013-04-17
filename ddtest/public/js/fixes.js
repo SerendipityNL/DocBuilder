9d03f9b064e361772549f743dfc0ecf644b25bc9
Douwe de Haan <douwedehaan@live.nl>
2013-04-17 09:02:09 +0200

    Click to add new blocks [Working]

--- ddtest/public/js/fixes.js
+++ ddtest/public/js/fixes.js
@@ -26,7 +26,44 @@ function restartSortable() {
 
 function BlockListener() {
 	$("#sortable1 div").on('click', function(e) {
-		$(this).toggleClass('emptyBlock filledBlock');
-		console.log('click');
+		if ($(this).hasClass('emptyBlock')) {
+			changeToFilled($(this));
+		}
+		else if ($(this).hasClass('filledBlock')) {
+			changeSize($(this));
+		}
 	});
+}
+
+function changeToFilled(el) {
+	el.toggleClass('emptyBlock filledBlock');
+	el.text('changed to filled');
+}
+
+function changeSize(el) {
+	if (el.hasClass('col_1')) {
+		el.removeClass('col_1');
+		el.addClass('col_2');
+	}
+	else if (el.hasClass('col_2')) {
+		el.removeClass('col_2');
+		el.addClass('col_3');
+	}
+	else if (el.hasClass('col_3')) {
+		el.removeClass('col_3');
+		el.addClass('col_4');
+	}
+	else {
+		el.removeClass('col_4');
+		el.addClass('col_1');
+	}
+	removeNextEmpty(el);
+}
+
+function removeNextEmpty(el) {
+	if (el.next().hasClass('emptyBlock')) {
+		if (el.position().top == el.next().position().top ) {
+			el.next().remove();
+		}
+	}
 }
