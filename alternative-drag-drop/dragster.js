7e810432e9e75228dc2a334d4896507201c0260d
Vincent Bremer <v-br@live.nl>
2013-06-18 22:21:50 +0200

    Drag and drop updates

--- alternative-drag-drop/dragster.js
+++ alternative-drag-drop/dragster.js
@@ -1,13 +1,42 @@
-$(document).bind('mousemove', function(e) {
+$.fn.draggable = function() {
+
+	var attached = false;
+	var container = $('#blocks');
 	
-	$('.move').css({
-		left: e.pageX,
-		top: e.pageY
+	$(this).on('click', function() {
+
+		
+		// container.height()
+		// container.width()
+		// container.offset().top
+		// container.offset().left
+	
+		var element = $(this);
+
+		if (attached) {
+			element.removeClass('move');
+			$(document).off('mousemove');
+			attached = false;
+		}
+		else {
+
+			element.addClass('move');
+			$(document).on('mousemove', function(e) {
+				element.offset({
+					left: e.pageX - (element.width() / 2),
+					top: e.pageY - (element.height() / 2)
+				});
+
+				//console.log(element.offset().top);
+
+			});
+			attached = true;
+		}
+		
 	});
-});
+}
+
 
 $(document).ready(function() {
-	$('#blocks > div').on('click', function() {
-		$(this).addClass('move');
-	});
+	$('#blocks > div').draggable();
 });
