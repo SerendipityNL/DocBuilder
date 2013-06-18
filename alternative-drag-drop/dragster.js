7a6578bc98634bf8dedbd62f2137eb750709a9f9
Vincent Bremer <vbremer89@gmail.com>
2013-06-19 01:05:25 +0200

    Setting the container for drag and drop

--- alternative-drag-drop/dragster.js
+++ alternative-drag-drop/dragster.js
@@ -1,38 +1,60 @@
 $.fn.draggable = function() {
 
-	var attached = false;
+	var dragging = false;
 	var container = $('#blocks');
+	var overflow = 25;
 	
 	$(this).on('click', function() {
-
 		
-		// container.height()
-		// container.width()
-		// container.offset().top
-		// container.offset().left
-	
 		var element = $(this);
 
-		if (attached) {
+		if (dragging) {
 			element.removeClass('move');
 			$(document).off('mousemove');
-			attached = false;
+			dragging = false;
 		}
 		else {
+			element.addClass('move');		
 
-			element.addClass('move');
+			var containerTop = container.offset().top;
+			var containerLeft = container.offset().left;
+			var containerHeight = container.height();
+			var containerWidth = container.width();
+
+			console.log(containerLeft);
+
+			var elementWidth = element.width();
+			var elementHeight = element.height();
+
 			$(document).on('mousemove', function(e) {
+
+				var elementTop = e.pageY - (elementHeight / 2);
+				var elementLeft = e.pageX - (elementWidth / 2);
+
+
+				if (elementLeft < containerLeft) {
+					elementLeft = containerLeft;
+				}
+				else if (elementLeft + elementWidth > containerLeft + containerWidth) {
+					//elementLeft = elementLeft + containerLeft; 368
+					
+				}
+				
+
+				if (elementTop < containerTop) {
+					elementTop = containerTop;
+				}
+				else if (elementTop + elementHeight > containerTop + containerHeight) {
+
+				}
+
 				element.offset({
-					left: e.pageX - (element.width() / 2),
-					top: e.pageY - (element.height() / 2)
+					left: elementLeft,
+					top: elementTop
 				});
-
-				//console.log(element.offset().top);
-
 			});
-			attached = true;
-		}
-		
+			dragging = true;
+		}		
 	});
 }
 
